f8f7f5c2130e455c271fe40d06ed7afe0bb761d6
Michael Cook <michaeljcook53@gmail.com>
2021-04-07 17:27:31 -0400

    Finished tip_calc2 problem

--- Python/box.py
+++ Python/box.py
@@ -4,7 +4,7 @@ Height = int(input("Height? "))
 
 for i in range(Width):
     for j in range(Height):
-        if i ==() or i ==Width-1 or j ==() or j ==Height-1:
+        if i == 0 or i ==(Width-1) or j == 0 or j ==(Height-1):
             print("*", end = "")
         else:
             print (" ", end = "")
--- Python/tip_calc2.py
+++ Python/tip_calc2.py
@@ -1,1 +1,27 @@
+Bill = float(input("The total bill amount? "))
 
+service = input("The level of service? ")
+
+split = int(input("Split how many ways? "))
+
+if service.lower() == "good":
+    tip = .20
+
+
+elif service.lower() == "fair":    
+    tip = .15
+
+
+elif service.lower() == "bad":
+    tip = .10
+    
+    
+result = Bill * tip
+
+total = result + Bill
+
+totalSplit = total / split
+
+print("Tip Amount:$%.2f" % result)
+print("Total Amount:$%.2f" % total)
+print("Amount per person: $%.2f" % totalSplit)
