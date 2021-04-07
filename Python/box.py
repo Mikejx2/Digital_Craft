52bc49c765903dccdc30392f6facc7a06fbad6eb
Michael Cook <michaeljcook53@gmail.com>
2021-04-07 18:40:38 -0400

    finished box

--- Python/box.py
+++ Python/box.py
@@ -1,11 +1,6 @@
-Width = int(input("Width? "))
-
-Height = int(input("Height? "))
-
-for i in range(Width):
-    for j in range(Height):
-        if i == 0 or i ==(Width-1) or j == 0 or j ==(Height-1):
-            print("*", end = "")
-        else:
-            print (" ", end = "")
+n = int(input('Width? '))
+m = int(input('Height? '))
+for row in range(m):
+    for col in range(n):
+        print('*' if row in(0,m-1) or col in(0,n-1) else ' ', end=' ')
     print()
