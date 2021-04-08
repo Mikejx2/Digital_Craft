d824ef9c7136262d09957555f7112dbb5f4df8c6
Michael Cook <michaeljcook53@gmail.com>
2021-04-07 20:54:27 -0400

    finished de-dup and long vowels

---
+++ Python/Homework01_3/LongLongVowels.py
@@ -0,0 +1,9 @@
+# Long-long Vowels
+# Given a word as a string, print the result of extending any long vowels to the length of 5.
+
+
+Word = "Game"
+
+Word = Word.replace("a", "aaaaa")
+
+print(Word)
