73b2182eec0fa53f2b5cc6af5d9756b4dc8db0d6
Michael Cook <michaeljcook53@gmail.com>
2021-04-13 16:10:34 -0400

    Phonebook Complete

---
+++ Python/Dictionary.py/phonebook.py
@@ -0,0 +1,70 @@
+import pickle
+
+# with open('phonebook.pickle', 'wb') as fh:
+#     pickle.dump(phonebook, fh)
+
+with open('phonebook.pickle', 'rb') as fh:
+    phonebook = pickle.load(fh)
+
+
+# Main
+
+userInput = ""
+
+
+
+
+
+
+
+#function
+def find():
+    findName = input("Name: ")
+    findPerson = phonebook[findName]
+    print(f"Phone Number: {findPerson}")
+    print(f"Found entry for {findName}")
+
+def add():
+    newName = input("Name: ")
+    newNumber = input("Phone Number: ")
+    phonebook[newName] = newNumber
+    with open('phonebook.pickle', 'wb') as fh:
+        pickle.dump(phonebook, fh)
+    print(f"Entry stored for {newName}")
+
+def delete():
+    delName = input("Name: ")
+    del phonebook[delName]
+    with open('phonebook.pickle', 'wb') as fh:
+        pickle.dump(phonebook, fh)
+    print(f"Deleted entry for {delName}")
+
+def displayAll():
+    print( phonebook)
+quitProgram = "5"
+
+
+while quitProgram == "5":
+    userInput = input('''
+Electronic Phone Book
+=====================
+1. Look up an entry
+2. Set an entry
+3. Delete an entry
+4. List all entries
+5. Quit
+What do you want to do (1-5)? ''')
+    choice = userInput
+    if choice == "1":
+        find()
+    elif choice == "2":
+        add()
+    elif choice == "3":
+        delete()
+    elif choice == "4":
+        displayAll()
+    elif choice == "5":
+        print("Bye.")
+        break
+    else:
+        print("Invaild selection!")
